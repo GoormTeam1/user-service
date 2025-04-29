635fa78dc718366812be126f4b0b903fc3a290b4
minseong1 <alstjd2627@naver.com>
2025-04-29 20:47:00 +0900

    chore(ErrorCode): 토큰 관련 에러코드 추가

--- src/main/java/edu/goorm/userservice/global/exception/ErrorCode.java
+++ src/main/java/edu/goorm/userservice/global/exception/ErrorCode.java
@@ -10,10 +10,13 @@ public enum ErrorCode {
   INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 입력입니다."),
   // 401
   UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
+  INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "만료되거나 토큰이 존재하지 않습니다."),
+  INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"비밀번호가 유효하지 않습니다."),
   // 403
   FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없습니다."),
   // 404
   ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 자원을 찾을 수 없습니다."),
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않는 유저입니다."),
   // 409
   DUPLICATE_RESOURCE(HttpStatus.CONFLICT, "이미 존재하는 자원입니다."),
   // 500
