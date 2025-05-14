09a02f02877680483270d772cf5ca6beb297ef7e
minseong1 <alstjd2627@naver.com>
2025-05-14 13:29:01 +0900

    fix: category 오타, 대문자 소문자로 수정

--- src/main/java/edu/goorm/userservice/domain/user/entity/Category.java
+++ src/main/java/edu/goorm/userservice/domain/user/entity/Category.java
@@ -3,5 +3,5 @@ package edu.goorm.userservice.domain.user.entity;
 import org.aspectj.weaver.World;
 
 public enum Category {
-  US, World, Pollitics, Business, Heallth, Entertainment, Style, Travel, Sports, Science, Climate, Weather
+  us, world, politics, business, health, entertainment, style, travel, sports, science, climate, weather
 }
